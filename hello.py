39c4c449560212a5f4f48eb2ca2c58fd25f41a3d
krishna bashyal <krishna.hello23@gmail.com>
2022-12-26 19:10:14 +0545

    operators and if

--- hello.py
+++ hello.py
@@ -1,7 +1,32 @@
-# yoo yoo
+# include <iostream> using namespace std;
+#include <maths>
+#jk lol sorry 
 print("lekhdina kei ne ")
 x=10
 m=20
 print(x+m)
 print ('\n')
 print (type(m))
+# swap variables
+x= x+m
+m= x-m
+x= x-m
+print (x,m)
+#method 2 lets go 
+a= 100
+b=12
+c= 50
+a,b = b,a
+print (a,b)
+#method 3? now youre just flexing your google search
+# a^=b
+# b^=a
+# a^=b
+# print (a,b)
+
+if a>b  and a>c:
+    print('a thulo ')
+elif  b>c :
+    print ('b thulo')
+else:
+    print('c thulo')
